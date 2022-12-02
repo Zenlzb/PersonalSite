961a0c79eee6d5da0c4aad822572d2ce518f58d5
Lee Zen Been <leezenbeen@yahoo.com.sg>
2022-12-02 21:03:55 +0800

    Dark mode debug

--- pages/index.vue
+++ pages/index.vue
@@ -12,5 +12,7 @@
     <h1 class="text-dark-blue dark:text-white text-center text-3xl mt-8">
       Hi! I'm Zen
     </h1>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <h1>Color preference: {{ $colorMode.preference }}</h1>
   </div>
 </template>
